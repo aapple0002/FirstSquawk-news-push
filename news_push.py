88f8542b7d7dc7f2874947747ce62b3f143f54b2
aapple0002 <aapple0002@outlook.com>
2026-01-15 20:53:31 +0800

    Update news_push.py

--- news_push.py
+++ news_push.py
@@ -1,4 +1,4 @@
-import feedparser
+2import feedparser
 import smtplib
 from email.mime.text import MIMEText
 import requests
@@ -15,7 +15,7 @@ SMTP_SERVER = "smtp.gmail.com"
 CUSTOM_NICKNAME = "📩全球快讯"
 
 # ---------------------- 基础配置（不用改） ----------------------
-RSS_URL = "https://rsshub.rssforever.com/sotwe/user/FirstSquawk?format=xml&.xml"
+RSS_URL = "https://rss.app/feeds/YnTegq50OCluW6vH.xml"
 LAST_LINK_FILE = "last_link.txt"
 REQUEST_HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
